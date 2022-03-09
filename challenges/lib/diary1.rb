f41f30a3893fe8ef8db0d6167041e662a3464306
laetitia <laetitia.studer@gmail.com>
2022-03-09 16:24:29 +0000

    challenge1

---
+++ challenges/lib/diary1.rb
@@ -0,0 +1,17 @@
+class Diary
+
+  def make_snippet(text)
+    my_text = text.split(" ")
+    if my_text.length >= 5
+      return my_text[0..4].join(" ") + "..."
+    else
+      return my_text[0..4].join(" ")
+    end 
+  end
+
+  def count_words(word)
+    return word.split(" ").length
+  end
+
+
+end
---
+++ challenges/spec/diary1_spec.rb
@@ -0,0 +1,62 @@
+require 'diary1'
+
+RSpec.describe Diary do
+
+=begin
+it "returns a string as an argument" do
+    diary = Diary.new
+    expect(diary.count_words("one two three four")).to eq "one two three four"
+  end 
+=end
+
+  it "returns the number of words in the string" do
+    diary = Diary.new
+    expect(diary.count_words("one two three four")).to eq 4
+  end
+end
+
+=begin
+  it "returns ... if more than five words" do
+    diary = Diary.new
+
+    expect(diary.make_snippet("one two three four five six")).to eq "one two three four five..."
+  end
+
+  it "returns the words without ... if less than five words" do
+   diary = Diary.new 
+
+   expect(diary.make_snippet("one two three")).to eq "one two three"
+  end
+end
+=end
+
+=begin
+  it "takes a string as an argument" do
+    diary = Diary.new
+
+    expect(diary.make_snippet("hello")).to eq "hello"
+  end
+=end
+
+=begin
+  it "returns the first five words" do
+    diary = Diary.new
+
+    expect(diary.make_snippet("one two three four five six")).to eq "one two three four five"
+    expect(diary.make_snippet("seven eight nine ten eleven twelve")).to eq "seven eight nine ten eleven"
+  end
+=end
+
+=begin
+  it "splits the string into an array" do
+    diary = Diary.new
+
+    expect(diary.make_snippet("one two three four five six")).to eq ["one", "two", "three", "four", "five", "six"]
+  end
+=end
+
+
+
+
+
+
